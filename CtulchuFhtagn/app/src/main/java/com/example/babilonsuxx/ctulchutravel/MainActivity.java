5252a1915711313e6e4d9491a2b2299382beee9a
babilonsuxx <babilonsuxx@gmail.com>
2017-09-11 19:47:05 +0300

    созданы фрагменты
    1.созданы фрагменты для каждого пункта меню
    2. добавлены в переключение в меню между ними
    3. сделана загрузка фрагмента с поиском при загрузке. (чтобы не было пустого экрана)

--- CtulchuFhtagn/app/src/main/java/com/example/babilonsuxx/ctulchutravel/MainActivity.java
+++ CtulchuFhtagn/app/src/main/java/com/example/babilonsuxx/ctulchutravel/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
 import android.view.View;
 
 
@@ -14,6 +16,11 @@ public class MainActivity extends AppCompatActivity   {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     TextView tv;
+    fragment_send fragment_send;
+    fragment_search fragment_search;
+    fragment_hot fragment_hot;
+    fragment_contact fragment_contact;
+    FragmentTransaction fragmentMenuTransaction;
 
 
     @Override
@@ -27,22 +34,30 @@ public class MainActivity extends AppCompatActivity   {
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        tv = (TextView) findViewById(R.id.textView);
+
+
+        // инициализируем фрагменты
+        fragment_send=new fragment_send();
+        fragment_contact=new fragment_contact();
+        fragment_search=new fragment_search();
+        fragment_hot=new fragment_hot();
+
+        //загружаем по умолчанию фрагмент с формой поиска
+        fragmentMenuTransaction=getFragmentManager().beginTransaction();
+        fragmentMenuTransaction.replace(R.id.fragment_container,fragment_search);
+        fragmentMenuTransaction.addToBackStack(null);
+        fragmentMenuTransaction.commit();
+
+        //
+
+      //  tv = (TextView) findViewById(R.id.textView);
 
     }
     //делаем чтобы закрывалось
     public  boolean onOptionsItemSelected(MenuItem item) {
-
-
         if (mToggle.onOptionsItemSelected(item)) {
-
-
-
-          // mDrawerLayout.closeDrawers();
             return true;
-
         }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -52,14 +67,37 @@ public class MainActivity extends AppCompatActivity   {
     {
         // выводим сообщение
         // Выведем в TextView информацию о нажатом пункте меню
-        StringBuilder sb = new StringBuilder();
-        sb.append("Item Menu");
-        sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
-        sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
-        sb.append("\r\n order: " + String.valueOf(item.getOrder()));
-        sb.append("\r\n title: " + item.getTitle());
+    //   StringBuilder sb = new StringBuilder();
+    //    sb.append("Item Menu");
+     //   sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+     //   sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+     //   sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+     //  sb.append("\r\n title: " + item.getTitle());
+
+
+        fragmentMenuTransaction=getFragmentManager().beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.search_menu:
+                fragmentMenuTransaction.replace(R.id.fragment_container,fragment_search);
+                break;
+            case R.id.hot_menu:
+                fragmentMenuTransaction.replace(R.id.fragment_container,fragment_hot);
+                break;
+            case R.id.send_menu:
+                fragmentMenuTransaction.replace(R.id.fragment_container,fragment_send);
+                break;
+            case R.id.contact_menu:
+                fragmentMenuTransaction.replace(R.id.fragment_container,fragment_contact);
+                break;
+            default:
+                break;
+        }
+        fragmentMenuTransaction.addToBackStack(null);
+        fragmentMenuTransaction.commit();
+
+
         mDrawerLayout.closeDrawers();
-        tv.setText(sb.toString());
+      //  tv.setText(sb.toString());
     }
 
 }
---
+++ CtulchuFhtagn/app/src/main/java/com/example/babilonsuxx/ctulchutravel/fragment_send.java
@@ -0,0 +1,20 @@
+package com.example.babilonsuxx.ctulchutravel;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by muz_on on 11.09.17.
+ */
+
+public class fragment_send extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
+        return  inflater.inflate(R.layout.fragment_send,null);
+    }
+}
